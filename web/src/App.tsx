0fed0326a88ca6fe7d74b17e716bbb544e995716
GeekWx <geekwx@gmail.com>
2021-01-29 06:19:18 -0300

    criando header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './assets/styles/global.css';
 
-import Landing from './pages/Landing';
+import Routes from './routes';
 
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Routes />
     </div>
   );
 }
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 
 import landingImg from '../../assets/images/landing.svg';
@@ -19,12 +19,12 @@ function Landing(){
                 </div>
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" /> Estudar 
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aula" /> Dar aula 
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
